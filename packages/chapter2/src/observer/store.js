248498f53738a22ef6cdb3c7a0e1bba2158c4b70
Yonghyun Kim <81367886+yonghyun421@users.noreply.github.com>
2024-03-28 23:43:16 +0900

    과제E

--- packages/chapter2/src/observer/store.js
+++ packages/chapter2/src/observer/store.js
@@ -1,6 +1,12 @@
+import { 발행기관 } from "./pubsub";
+
 export class Store {
-  constructor({ state, mutations, actions }) {}
+  constructor({ state, mutations, actions }) {
+    this.state = 발행기관(state);
+    this.mutations = mutations;
+  }
 
-  commit(action, payload) {}
-
+  commit(action, payload) {
+    this.mutations[action](this.state, payload);
+  }
 }
