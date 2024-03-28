477d8c417cd743937fd80c84fa0539afc8ae4560
Yonghyun Kim <81367886+yonghyun421@users.noreply.github.com>
2024-03-28 23:30:21 +0900

    과제E_김용현

--- packages/chapter2/src/observer/pubsub.js
+++ packages/chapter2/src/observer/pubsub.js
@@ -1,7 +1,28 @@
-export const 구독 = fn => {
+let currentCallback = null;
+export const 구독 = (fn) => {
+  currentCallback = fn;
   fn();
-}
+  currentCallback = null;
+};
 
-export const 발행기관 = obj => {
-  return obj;
-}
+export const 발행기관 = (obj) => {
+  const state = {};
+
+  Object.keys(obj).forEach((key) => {
+    let _value = obj[key];
+    const 구독자들 = new Set();
+
+    Object.defineProperty(state, key, {
+      get() {
+        currentCallback && 구독자들.add(currentCallback);
+        return _value;
+      },
+      set(value) {
+        _value = value;
+        구독자들.forEach((fn) => fn());
+      },
+    });
+  });
+
+  return state;
+};
